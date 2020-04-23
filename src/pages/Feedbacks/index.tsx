2a3a098aef2e7395d80024b1e6684a828e6d3d2b
Jamal Mashal <jamal.mashaal@gmail.com>
2020-04-23 07:52:37 -0700

    remote widget comp

--- src/pages/Feedbacks/index.tsx
+++ src/pages/Feedbacks/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Feedback from '../../components/Feedback';
 import FeedbackList from '../../components/FeedbackList';
 
 function App() {
    return (
       <div className="App">
          <FeedbackList />
-         <Feedback />
       </div>
    );
 }
