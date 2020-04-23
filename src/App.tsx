2ef292fec69225e3300667f74a0882ac1e43abc1
Jamal Mashal <jamal.mashaal@gmail.com>
2020-04-23 08:56:47 -0700

    layout update

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ import {
   Switch,
   Route,
   Link,
-  Redirect
+  Redirect,
+  useLocation,
 } from "react-router-dom";
 
 import Feedbacks from './pages/Feedbacks'
@@ -74,92 +75,97 @@ function PublicRoute({ children, ...rest }: any) {
   );
 }
 
-const BasicExample = () => {
+const App = () => {
 
   const [collapsed, setCollapsed] = useState(false)
   const { loading, error, data } = useQuery(USER, {})
+
+  let location = useLocation();
 
   const onCollapse = (collapsed: boolean) => {
     setCollapsed(collapsed);
   };
 
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
+          {collapsed ? <span style={{ fontSize: 'x-large' }}>🦨</span> : 'Moufette v0.1'}
+        </div>
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['/']} selectedKeys={[location.pathname]}>
+          <Menu.Item key="/">
+            <Link to="/">
+              <HomeOutlined />
+              <span>Home</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/feedbacks">
+            <Link to="/feedbacks">
+              <MessageOutlined />
+              <span>Feedbacks</span>
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="/setup">
+            <Link to="/setup">
+              <SettingOutlined />
+              <span>Setup</span>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout" style={{ height: '100vh' }}>
+        <Header className="site-layout-background" style={{ padding: 0 }}>
+          <Row justify="space-between" align="middle">
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: () => onCollapse(!collapsed),
+            } as any)}
+
+            <span style={{ paddingRight: 24 }}>
+              <UserDropdown currentUser={data?.currentUser} />
+            </span>
+
+
+          </Row>
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            overflow: 'auto'
+          }}
+        >
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/feedbacks">
+            <Feedbacks />
+          </Route>
+
+          <Route path="/setup">
+            <Setup currentUser={data?.currentUser} />
+          </Route>
+        </Content>
+      </Layout>
+    </Layout>
+
+  )
+}
+
+const BasicExample = () => {
 
   return (
     <Router>
-
       <Switch>
-
         <PublicRoute path="/login">
           <Login />
         </PublicRoute>
 
         <PrivateRoute>
-
-          <Layout style={{ minHeight: '100vh' }}>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
-              <div className="logo">
-                {collapsed ? <span style={{ fontSize: 'x-large' }}>🦨</span> : 'Moufette v0.1'}
-              </div>
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                <Menu.Item key="1">
-                  <Link to="/">
-                    <HomeOutlined />
-                    <span>Home</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <Link to="/feedbacks">
-                    <MessageOutlined />
-                    <span>Feedbacks</span>
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Link to="/setup">
-                    <SettingOutlined />
-                    <span>Setup</span>
-                  </Link>
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout className="site-layout" style={{height: '100vh'}}>
-              <Header className="site-layout-background" style={{ padding: 0 }}>
-                <Row justify="space-between" align="middle">
-                  {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                    className: 'trigger',
-                    onClick: () => onCollapse(!collapsed),
-                  } as any)}
-
-                  <span style={{ paddingRight: 24 }}>
-                    <UserDropdown currentUser={data?.currentUser} />
-                  </span>
-
-
-                </Row>
-              </Header>
-              <Content
-                className="site-layout-background"
-                style={{
-                  margin: '24px 16px',
-                  padding: 24,
-                  minHeight: 280,
-                  overflow: 'auto'
-                }}
-              >
-                <Route exact path="/">
-                  <Home />
-                </Route>
-
-                <Route path="/feedbacks">
-                  <Feedbacks />
-                </Route>
-
-                <Route path="/setup">
-                  <Setup currentUser={data?.currentUser} />
-                </Route>
-              </Content>
-            </Layout>
-          </Layout>
-
+          <App />
         </PrivateRoute>
       </Switch>
     </Router>
